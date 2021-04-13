5d1d01e6a993289c66049725251ca88e5b77216b
gregoriofracassi <gregorio.fracassi@gmail.com>
2021-04-13 16:54:19 +0200

    added previews

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem", {
             card.classList.add('card')
             card.classList.add('m-3')
             card.style.width = '12rem'
-            card.innerHTML = `<img class="card-img-top" src="${track.album.cover}" alt="Card image cap"><div class="card-body"><p class="card-text"><b>${track.title}</b></p><div>${track.artist.name}</div></div>`
+            card.innerHTML = `<img class="card-img-top" src="${track.album.cover}" 
+            alt="Card image cap"><div class="card-body"><a href="${track.preview}"><p class="card-text"><b>${track.title}
+            </b></p></a><div>${track.artist.name}</div></div>`
             col.appendChild(card)
         }
     )
